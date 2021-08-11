1fd0cf0e83c45ce5b6291f7d85e000ef246fda1a
Indefn <73712175+sr-indefn@users.noreply.github.com>
2021-08-11 13:23:58 +0400

    structural changes

--- source/server/index.js
+++ source/server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 
+const clientDir = path.join(__dirname, '../client');
+
 express()
-    .use(express.static(path.join(`${__dirname}/public`)))
+    .use(express.static(clientDir))
     .listen(process.env.PORT || 80);
